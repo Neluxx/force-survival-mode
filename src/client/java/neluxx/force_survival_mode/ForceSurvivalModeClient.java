c0b481f4890c2ef0487963dca11a8dae4cfd1008
fabian.arndt <fabian.arndt@orca.ch>
2025-10-18 15:23:25 +0200

    Migrate datapack to fabric mod

---
+++ src/client/java/neluxx/force_survival_mode/ForceSurvivalModeClient.java
@@ -0,0 +1,10 @@
+package neluxx.force_survival_mode;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForceSurvivalModeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
