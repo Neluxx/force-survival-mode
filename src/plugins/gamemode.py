0d0e1d3ecdd52619888391187bb0a9d78ba9435a
Neluxx <fabian.arndt96@proton.me>
2026-04-10 12:58:03 +0200

    feat: add beet plugin to add JSON and .mcfunction files for each version

---
+++ src/plugins/gamemode.py
@@ -0,0 +1,18 @@
+from beet import Context, DataPack, Function, FunctionTag
+
+FUNCTION_ID = "no_creative_mode:trigger"
+COMMANDS = ["gamemode survival @a"]
+
+
+def beet_default(ctx: Context):
+    apply_patch(ctx.data)
+
+    overlay_versions = ctx.meta.get("overlay_versions", {})
+    for directory in overlay_versions:
+        overlay = ctx.data.overlays[directory]
+        apply_patch(overlay)
+
+
+def apply_patch(pack: DataPack):
+    pack[Function][FUNCTION_ID] = Function(COMMANDS)
+    pack["minecraft:tick"] = FunctionTag({"values": ["no_creative_mode:trigger"]})
