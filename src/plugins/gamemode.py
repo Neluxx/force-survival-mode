ad9059b891511aba5fb6d5277f355fad7bc57e47
Neluxx <fabian.arndt96@proton.me>
2026-04-13 21:09:08 +0200

    refactor: use utility class for beet plugin

--- src/plugins/gamemode.py
+++ src/plugins/gamemode.py
@@ -1,18 +1,12 @@
-from beet import Context, DataPack, Function, FunctionTag
+from beet import Context, Function, FunctionTag
+
+from src.plugins.utils import iterate_versions
 
 FUNCTION_ID = "no_creative_mode:trigger"
 COMMANDS = ["gamemode survival @a"]
 
 
 def beet_default(ctx: Context):
-    apply_patch(ctx.data)
-
-    overlay_versions = ctx.meta.get("overlay_versions", {})
-    for directory in overlay_versions:
-        overlay = ctx.data.overlays[directory]
-        apply_patch(overlay)
-
-
-def apply_patch(pack: DataPack):
-    pack[Function][FUNCTION_ID] = Function(COMMANDS)
-    pack["minecraft:tick"] = FunctionTag({"values": [FUNCTION_ID]})
+    for pack, version in iterate_versions(ctx):
+        pack[Function][FUNCTION_ID] = Function(COMMANDS)
+        pack["minecraft:tick"] = FunctionTag({"values": [FUNCTION_ID]})
